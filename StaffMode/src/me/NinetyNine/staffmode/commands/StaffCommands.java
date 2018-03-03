d59747aae0c403cda4fe804db7868db2869347a4
BuildTools <unconfigured@null.spigotmc.org>
2018-03-03 15:00:14 +0800

    4th commit

--- StaffMode/src/me/NinetyNine/staffmode/commands/StaffCommands.java
+++ StaffMode/src/me/NinetyNine/staffmode/commands/StaffCommands.java
@@ -97,12 +97,11 @@ public class StaffCommands implements CommandExecutor, Listener {
 			if (player.hasPermission("staffmode.clear")) {
 				// edited, thx
 				for (int i = 0; i < 100; i++) {
-					Bukkit.getServer().broadcastMessage("\n" + ChatColor.DARK_RED
-									+ "=============================================\n" + ChatColor.DARK_PURPLE
-									+ "The server chat has been cleared by " + ChatColor.DARK_RED
-									+ player.getPlayer().getName() + ChatColor.DARK_PURPLE + "!" + ChatColor.DARK_RED
-									+ "=============================================\n");
+					Bukkit.getServer().broadcastMessage("\n");
 				}
+				Bukkit.getServer().broadcastMessage("§4=============================================\n"
+						+ "§5The server chat has been cleared by " + ChatColor.DARK_RED + player.getPlayer().getName()
+						+ "§5!" + "§4=============================================\n");
 			} else {
 				player.sendMessage("§1[§4StaffMode§1] §l§cYou do not have permissions to use this!");
 			}
